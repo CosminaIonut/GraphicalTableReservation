35bf6263cbb60eda34a0047fd2916cfd0d32dac7
CosminaIonut <62280428+CosminaIonut@users.noreply.github.com>
2020-11-13 22:10:01 +0200

    Booking page desing timePicker and datePicker

---
+++ RestaurantReservation/app/src/main/java/com/example/restaurantreservation/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.restaurantreservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //myDb = new DatabaseHelper(this);
+        //myDb.insertData();
+        Button buttonOne = findViewById(R.id.buttonOne);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                Intent mapActivity = new Intent(getApplicationContext(), RestaurantHomePage.class);
+                startActivity(mapActivity);
+            }
+        });
+    }
+}
