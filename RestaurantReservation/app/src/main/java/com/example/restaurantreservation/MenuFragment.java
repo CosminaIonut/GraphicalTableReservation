bd63a560d230a84e8b6afb389cb3e287b90bd0c2
CosminaIonut <62280428+CosminaIonut@users.noreply.github.com>
2020-12-02 15:42:31 +0200

    updates

--- RestaurantReservation/app/src/main/java/com/example/restaurantreservation/MenuFragment.java
+++ RestaurantReservation/app/src/main/java/com/example/restaurantreservation/MenuFragment.java
@@ -10,16 +10,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TableLayout;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.PagerAdapter;
+
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
@@ -35,12 +29,6 @@ public class MenuFragment extends Fragment {
     private ViewPager viewPager;
     private TabItem drinks, food;
     public PageAdapter pagerAdapter;
-    CustomAdapter customAdapter;
-    DatabaseHelper myDb;
-    ListView listView;
-    ArrayList<String> item_id,item_name, item_desc, item_price , item_quantity, item_sortiment;
-    ArrayAdapter adapter;
-    RecyclerView recyclerView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -77,22 +65,7 @@ public class MenuFragment extends Fragment {
               }
           });
           viewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-//
-////          listItem=new ArrayList<>();
-//          recyclerView=view.findViewById(R.id.menuList);
-////          listView=view.findViewById(R.id.menuList);
-//          myDb = new DatabaseHelper(getContext());
-//          item_id=new ArrayList<>();
-//          item_name=new ArrayList<>();
-//          item_desc=new ArrayList<>();
-//          item_quantity=new ArrayList<>();
-//          item_price=new ArrayList<>();
-//          item_sortiment=new ArrayList<>();
-//           storeDataInArray();
-//         RecyclerView.LayoutManager recyce = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
-//             recyclerView.setLayoutManager(recyce);
-//           customAdapter=new CustomAdapter(getContext(),item_id,item_name,item_desc,item_price,item_quantity,item_sortiment);
-//           recyclerView.setAdapter(customAdapter);
+
            btnNavFrgMenu.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
@@ -110,41 +83,8 @@ public class MenuFragment extends Fragment {
 
           return view;
     }
-        public void showMessage(String title, String  message){
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder. setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
-    }
-    public void storeDataInArray() {
-        int restaurantId = 1; // acum hardcodat da o sa vina in functie de restaurant
-        Cursor result = myDb.getallFoodFromMenu(restaurantId);
-        if (result.getCount() == 0) {
-            showMessage("Error", "Keine Tupel in der Tabelle");
 
-        } else {
-            while (result.moveToNext()) {
-
-                String itemID = result.getString(2);
-                String itemName = result.getString(3);
-                String itemDescription = result.getString(4);
-                String price = result.getString(5);
-                String quantity = result.getString(6);
-                String type = result.getString(7);
-                String sortiment = result.getString(8);
-//                listItem.add(itemName);
-                item_id.add(itemID);
-                item_name.add(itemName);
-                item_desc.add(itemDescription);
-                item_price.add(price);
-                item_sortiment.add(sortiment);
-                item_quantity.add(quantity);
-
-            }
-        }
-    }
 
 
 }
