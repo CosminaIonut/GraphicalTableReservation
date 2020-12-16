4fc3f49435ede4b58b85b75cbf1b2dc96b6e4dbf
miv42 <62116681+miv42@users.noreply.github.com>
2020-12-16 15:02:22 +0200

    removed slide animation on restaurantHomePage

--- RestaurantReservation/app/src/main/java/com/example/restaurantreservation/RestaurantHomePage.java
+++ RestaurantReservation/app/src/main/java/com/example/restaurantreservation/RestaurantHomePage.java
@@ -45,6 +45,7 @@ public class RestaurantHomePage extends AppCompatActivity {
         SectionStatePagerAdapter adapter= new SectionStatePagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         adapter.addFragemnt(new RestaurantFragment(), "RestaurantFragemnt");
         adapter.addFragemnt(new MenuFragment(), "MenuFragemnt");
+        viewPager.setPageTransformer(false, new FadePageTransformer());
         viewPager.setAdapter(adapter);
     }
 
@@ -55,6 +56,19 @@ public class RestaurantHomePage extends AppCompatActivity {
         mViewPager.setCurrentItem(fragemntNumber);
     }
 
+    public class FadePageTransformer implements ViewPager.PageTransformer {
+        public void transformPage(View view, float position) {
+            view.setTranslationX(view.getWidth() * -position);
 
+            if(position <= -1.0F || position >= 1.0F) {
+                view.setAlpha(0.0F);
+            } else if( position == 0.0F ) {
+                view.setAlpha(1.0F);
+            } else {
+                // position is between -1.0F & 0.0F OR 0.0F & 1.0F
+                view.setAlpha(1.0F - Math.abs(position));
+            }
+        }
+    }
 
 }
